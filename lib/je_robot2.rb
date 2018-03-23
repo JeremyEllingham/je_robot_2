e615e875779d8e9bef108b606f5aa79c3de31cf7
J E <jemtrash@gmail.com>
2018-03-23 11:21:40 +1100

    Initial move tests.

--- lib/je_robot2.rb
+++ lib/je_robot2.rb
@@ -1,4 +1,5 @@
 require "je_robot2/version"
+require 'je_robot2/robot'
 
 module JeRobot2
   # Your code goes here...
---
+++ lib/je_robot2/robot.rb
@@ -0,0 +1,18 @@
+require 'je_robot2/version'
+
+module JeRobot2
+  class Robot
+
+    def initialize(position = 0)
+      @position = position
+    end
+
+    def move
+    end
+
+    def position
+      3
+    end
+
+  end
+end

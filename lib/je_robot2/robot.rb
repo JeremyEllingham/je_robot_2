880738181b87f9580570577ed9bd411f8e1e56db
J E <jemtrash@gmail.com>
2018-03-23 11:32:42 +1100

    Add movement west.

--- lib/je_robot2/robot.rb
+++ lib/je_robot2/robot.rb
@@ -3,15 +3,18 @@ require 'je_robot2/version'
 module JeRobot2
   class Robot
 
+    attr_reader :position
+
     def initialize(position = 0)
       @position = position
     end
 
-    def move
+    def move_east
+      @position += 1
     end
 
-    def position
-      3
+    def move_west
+      @position -= 1
     end
 
   end

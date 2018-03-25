87f4ebc5f5776d715683dbe9bbfe74dc0a3ca8fd
J E <jemtrash@gmail.com>
2018-03-25 21:56:32 +1100

    Add MOVE command.

--- lib/je_robot2/command.rb
+++ lib/je_robot2/command.rb
@@ -4,8 +4,11 @@ module JeRobot2
   class Command
 
     def self.process(command)
-      if match = /\APLACE (?<x>\d+),(?<y>\d+),(?<facing>\w+)\Z/.match(command)
-        [:place, match[:x].to_i, match[:y].to_i, match[:facing]]
+      case command
+      when /\APLACE (?<x>\d+),(?<y>\d+),(?<facing>\w+)\Z/
+        [:place, $~[:x].to_i, $~[:y].to_i, $~[:facing]]
+      when /\AMOVE\Z/
+        [:move]
       else
         [:invalid, command]
       end
--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -4,16 +4,24 @@ describe JeRobot2::Command do
   subject { JeRobot2::Command.new }
 
   context 'PLACE' do
-    it 'processes a PLACE command' do
+    it 'accepts a PLACE command' do
       command, *args = JeRobot2::Command.process("PLACE 1,2,NORTH")
       expect(command).to eq(:place)
       expect(args).to eq([1, 2, 'NORTH'])
     end
+  end
 
     it 'returns :invalid for an invalid PLACE command' do
       command = JeRobot2::Command.process("PLACE 1")
       expect(command).to eq([:invalid, "PLACE 1"])
     end
-  end
+
+    context 'MOVE' do
+      it 'accepts a MOVE command' do
+        command, *args = JeRobot2::Command.process("MOVE")
+        expect(command). to eq(:move)
+        expect(args).to be_empty
+      end
+    end
 
 end

02ab0dec8ba6b224e86a789f6d487ef74aa3a2f0
J E <jemtrash@gmail.com>
2018-03-25 21:20:37 +1100

    Add valid_placement? method.

--- lib/je_robot2/table.rb
+++ lib/je_robot2/table.rb
@@ -2,5 +2,21 @@ require 'je_robot2/version'
 
 module JeRobot2
   class Table
+
+    def initialize(height, width)
+      @height = height
+      @width = width
+    end
+
+    # Return valid placement if both x and y within table dimensions
+    # y needs 1 subtracted as table indexing starts at 0
+    def valid_placement?(x, y)
+      if (0..@height).include?(x) && (0..(@width-1)).include?(y)
+        return true
+      else
+        return false
+      end
+    end
+
   end
 end
